55e748b4174a01b77c8b6b1353d77b09e4f928ef
Astekin <isadogruca@hotmail.com>
2024-11-18 14:09:00 +0300

    Create and complete Register

---
+++ FinShark/api/Migrations/20241118104132_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "5048f0f7-b9c4-4577-8913-8cc3beaed4a4", null, "User", "USER" },
+                    { "883aed77-58e4-441f-b75e-29d69aa8ea67", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5048f0f7-b9c4-4577-8913-8cc3beaed4a4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "883aed77-58e4-441f-b75e-29d69aa8ea67");
+        }
+    }
+}
